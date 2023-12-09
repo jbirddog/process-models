5664fefbcecff7833008c71cbe2d3e8907a98590
Jon Herron <jon.herron@yahoo.com>
2023-12-09 12:00:41 -0500

    Resolve all called elements for a process

--- scripts/gen.py
+++ scripts/gen.py
@@ -1,3 +1,5 @@
+import json
+
 from dataclasses import dataclass
 from glob import glob
 from typing import Any
@@ -10,14 +12,29 @@ class ParsingContext:
     process_ids_by_file: dict[str, list[str]]
     file_by_process_id: dict[str, str]
     process_spec_by_id: dict[str, Any]
-    called_elements_by_process_id: dict[str, list[str]]
+    called_element_ids_by_process_id: dict[str, list[str]]
 
-def find_called_elements(process_id, process_spec, ctx):
-    called_elements = []
+def extend_called_element_ids(ctx):
+    def extend(ids, resolved):
+        extended = [*ids]
+        for id in ids:
+            if id not in resolved:
+                resolved[id] = extend(ctx.called_element_ids_by_process_id.get(id, []), resolved)
+            extended.extend(resolved[id])
+        extended = list(set(extended))
+        return extended
+    resolved = {}
+    for k, v in ctx.called_element_ids_by_process_id.items():
+        extended = extend(v, resolved)
+        ctx.called_element_ids_by_process_id[k] = extended
+    
+def find_called_element_ids(process_id, process_spec, ctx):
+    called_element_ids = []
     for task_spec in process_spec.task_specs.values():
         if isinstance(task_spec, CallActivity):
-            called_elements.append(task_spec.spec)
-    ctx.called_elements_by_process_id[process_id] = called_elements
+            called_element_ids.append(task_spec.spec)
+    if len(called_element_ids) > 0:
+        ctx.called_element_ids_by_process_id[process_id] = called_element_ids
 
 def parse_bpmn_file(bpmn_file, ctx):
     parser = SpiffBpmnParser()
@@ -33,19 +50,21 @@ def parse_bpmn_file(bpmn_file, ctx):
         
         ctx.file_by_process_id[process_id] = bpmn_file
         ctx.process_spec_by_id[process_id] = process_spec
-        find_called_elements(process_id, process_spec, ctx)
+        find_called_element_ids(process_id, process_spec, ctx)
         
-
 def generate_manifest_and_workflows(bpmn_files):
     ctx = ParsingContext(
         process_ids_by_file = {},
         file_by_process_id = {},
         process_spec_by_id = {},
-        called_elements_by_process_id = {},
+        called_element_ids_by_process_id = {},
     )
     
     for bpmn_file in bpmn_files:
         parse_bpmn_file(bpmn_file, ctx)
+
+    extend_called_element_ids(ctx)
+    print(json.dumps(ctx.called_element_ids_by_process_id, sort_keys=True, indent=2))
 
 if __name__ == "__main__":
     bpmn_files = glob("bpmn/**/*.bpmn")
