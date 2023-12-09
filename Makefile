ec6524ced3d72b61bffaa5a52e59a04653f77428
Jon Herron <jon.herron@yahoo.com>
2023-12-09 08:32:28 -0500

    Remove bad process model, work on building workflow specs and the manifest

--- Makefile
+++ Makefile
@@ -1,20 +1,19 @@
 DOCKER_IMG := process-models
-
 IN_DEV := docker run -it -v .:/app $(DOCKER_IMG)
 
-all: img-gen
+all: img-start
 
 img:
 	docker build -t $(DOCKER_IMG) .
 
-gen:
+start:
 	$(IN_DEV) python scripts/gen.py
 
-img-gen: img gen
+img-start: img start
 	@/bin/true
 
 sh:
 	$(IN_DEV) /bin/sh
 
-.PHONY: img gen img-gen \
+.PHONY: img start img-start \
 	sh
--- scripts/gen.py
+++ scripts/gen.py
@@ -1,2 +1,52 @@
+from dataclasses import dataclass
+from glob import glob
+from typing import Any
+
+from SpiffWorkflow.spiff.parser.process import SpiffBpmnParser
+from SpiffWorkflow.spiff.specs.defaults import CallActivity
+
+@dataclass
+class ParsingContext:
+    process_ids_by_file: dict[str, list[str]]
+    file_by_process_id: dict[str, str]
+    process_spec_by_id: dict[str, Any]
+    called_elements_by_process_id: dict[str, list[str]]
+
+def find_called_elements(process_id, process_spec, ctx):
+    called_elements = []
+    for task_spec in process_spec.task_specs.values():
+        if isinstance(task_spec, CallActivity):
+            called_elements.append(task_spec.spec)
+    ctx.called_elements_by_process_id[process_id] = called_elements
+
+def parse_bpmn_file(bpmn_file, ctx):
+    parser = SpiffBpmnParser()
+    parser.add_bpmn_files([bpmn_file])
+    process_ids = parser.get_process_ids()
+    ctx.process_ids_by_file[bpmn_file] = process_ids
+
+    for process_id in process_ids:
+        if process_id in ctx.file_by_process_id:
+            raise Exception(f"Duplicate process_id: {process_id}")
+
+        process_spec = parser.get_spec(process_id, required=False)
+        
+        ctx.file_by_process_id[process_id] = bpmn_file
+        ctx.process_spec_by_id[process_id] = process_spec
+        find_called_elements(process_id, process_spec, ctx)
+        
+
+def generate_manifest_and_workflows(bpmn_files):
+    ctx = ParsingContext(
+        process_ids_by_file = {},
+        file_by_process_id = {},
+        process_spec_by_id = {},
+        called_elements_by_process_id = {},
+    )
+    
+    for bpmn_file in bpmn_files:
+        parse_bpmn_file(bpmn_file, ctx)
+
 if __name__ == "__main__":
-    print("gen")
+    bpmn_files = glob("bpmn/**/*.bpmn")
+    generate_manifest_and_workflows(bpmn_files)
