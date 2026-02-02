e9cdc61104ccae49e2c01f6c8970262cd7f1b268
Jon Herron <jon.herron@yahoo.com>
2026-02-02 08:14:48 -0500

    wip

--- test.py
+++ test.py
@@ -10,9 +10,21 @@
 
 from spiff_arena_common.runner import advance_workflow, specs_from_xml
 
-def main():
-    print("Hello from test.py!")
+cases = {
+    "tests/basic-example.bpmn": [
+        "bpmn/test-cases/dict-tests/dict-tests.bpmn",
+    ],
+}
 
+def slurp(file):
+    with open(file) as f:
+        return f.read()
+
+def main():
+    for t, deps in cases.items():
+        files = [(t, slurp(t))] + [(d, slurp(d)) for d in deps]
+        specs, err = specs_from_xml(files)
+        r = advance_workflow(specs, {}, None, "greedy", None)
 
 if __name__ == "__main__":
     main()
