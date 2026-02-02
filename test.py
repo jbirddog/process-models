ab0215a3c1d7b8736a61289fea6ca9d0ab83c887
Jon Herron <jon.herron@yahoo.com>
2026-02-02 10:07:05 -0500

    wip

--- test.py
+++ test.py
@@ -14,11 +14,18 @@ import unittest
 from spiff_arena_common.runner import advance_workflow, specs_from_xml
 
 class BpmnTestCase(unittest.TestCase):
-    def __init__(self, t, deps):
-        self.t = t
-        self.deps = deps
+    def __init__(self, specs):
+        self.specs = specs
+        super().__init__()
 
-suite = {
+    def runTest(self):
+        r = json.loads(advance_workflow(self.specs, {}, None, "greedy", None))
+        assert r["status"] == "ok"
+        assert r["completed"]
+        test_result = r["result"]["test_result"]
+        assert test_result["failures"] == 0
+
+cases = {
     "tests/basic-example.bpmn": [
         "bpmn/test-cases/dict-tests/dict-tests.bpmn",
     ],
@@ -29,15 +36,22 @@ def slurp(file):
         return f.read()
 
 def main():
-    for t, deps in suite.items():
+    suite = unittest.TestSuite()
+    #r = unittest.TestResult()
+    for t, deps in cases.items():
         files = [(t, slurp(t))] + [(d, slurp(d)) for d in deps]
         specs, err = specs_from_xml(files)
-        r = json.loads(advance_workflow(specs, {}, None, "greedy", None))
-        print(r)
-        assert r["status"] == "ok"
-        assert r["completed"]
-        test_result = r["result"]["test_result"]
-        assert test_result["failures"] == 0
+        assert not err
+        suite.addTest(BpmnTestCase(specs))
+    #suite.run(r)
+    unittest.TextTestRunner().run(suite)    
 
+#s = unittest.defaultTestLoader.loadTestsFromTestCase(TestTaskData)
+#r = unittest.TestResult()
+#s.run(r)
+#print(r.testsRun)
+#print(r.failures)
+        
 if __name__ == "__main__":
     main()
+    
