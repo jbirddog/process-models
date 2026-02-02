64886f3d3481940127c224e9eaa32ad5139557da
Jon Herron <jon.herron@yahoo.com>
2026-02-02 07:53:13 -0500

    test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "jinja2>=3.1.6",
+#     "jsonschema>=4.26.0",
+#     "spiff-arena-common==0.1.6",
+#     "spiffworkflow==3.1.2",
+# ]
+# ///
+
+from spiff_arena_common.runner import advance_workflow, specs_from_xml
+
+def main():
+    print("Hello from test.py!")
+
+
+if __name__ == "__main__":
+    main()
