96d29bd360498ec8eeda1cdf05c0efa5a718b8a6
Jon Herron <jon.herron@yahoo.com>
2026-02-09 12:54:10 -0500

    wip

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from spiff_arena_common.coverage import task_coverage
 from spiff_arena_common.tester import run_tests_in_dir
 
 if __name__ == "__main__":
-    [ctx, result, output] = run_tests_in_dir(".")
+    ctx, result, output = run_tests_in_dir(".")
     output = [t.output for t in ctx.test_cases if not t.wasSuccessful and t.output] + [output]
     print(output[0])
 
@@ -25,10 +25,10 @@ if __name__ == "__main__":
     
     cov, tally = task_coverage(ctx)
     for id, f in ctx.files:
-        [completed, all, percent] = tally.breakdown[id]
+        completed, all, percent = tally.breakdown[id]
         print(f"{f} - {completed}/{all} - {percent:.2f}%")
     
-    [completed, all, percent] = tally.result
+    completed, all, percent = tally.result
     print(f"\nTotal - {completed}/{all} - {percent:.2f}%")
 
     if percent < 20.0:
