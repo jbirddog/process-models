459ffd718a76ef09417abd0887267a5458d1769e
jbirddog <100367399+jbirddog@users.noreply.github.com>
2026-02-27 10:24:17 -0500

    Add json file data store support to tests (#32)

--- test.py
+++ test.py
@@ -3,14 +3,36 @@
 # dependencies = [
 #     "jinja2>=3.1.6",
 #     "jsonschema>=4.26.0",
-#     "spiff-arena-common==0.1.8",
+#     "spiff-arena-common==0.1.9",
 #     "spiffworkflow==3.1.2",
 # ]
 # ///
 
+import json
 import sys
 
 from spiff_arena_common.coverage import task_coverage
+from spiff_arena_common.data_stores import JSONFileDataStore, JSONFileDataStoreConverter
+from SpiffWorkflow.spiff.serializer.config import SPIFF_CONFIG
+
+class JSONFileDataStoreDelegate:
+    def get(self, bpmn_id, my_task):
+        with open(f"examples/{bpmn_id}.json") as f:
+            data = json.loads(f.read())
+        return data, None
+    def set(self, bpmn_id, my_task, data):
+        with open(f"examples/{bpmn_id}.json", "w") as f:
+            f.write(json.dumps(data))
+        return None
+
+class MyJSONFileDataStoreConverter(JSONFileDataStoreConverter):
+    def from_dict(self, dct):
+        ds = super().from_dict(dct)
+        ds.delegate = JSONFileDataStoreDelegate()
+        return ds
+
+SPIFF_CONFIG[JSONFileDataStore] = MyJSONFileDataStoreConverter
+
 from spiff_arena_common.tester import run_tests_in_dir
 
 if __name__ == "__main__":
